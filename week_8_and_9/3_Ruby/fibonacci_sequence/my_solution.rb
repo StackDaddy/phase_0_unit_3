e97327c00d93ea2ed0a22700e02e70bd260d6c5a
Ryan Stack <rms332@yahoo.com>
2014-05-20 13:32:44 -0700

    all

--- week_8_and_9/3_Ruby/fibonacci_sequence/my_solution.rb
+++ week_8_and_9/3_Ruby/fibonacci_sequence/my_solution.rb
@@ -4,12 +4,27 @@
 # I worked on this challenge [by myself, with: ].
 
 # 2. Pseudocode
+#Input: A number 
+#Output A boolean, true or false 
+#Create array that stores the first three value of the fib sequence 
+#While the last number in this sequence is still less than the number provided, we want to check to see if that number
+#is within the array, and if it is not, we will continue enlarging the sequence. 
 
 
 
 # 3. Initial Solution
 
 def is_fibonacci?(num)
+	fib_array = [0, 1, 1] 
+	while num >= fib_array[-1] 
+		if fib_array.include?(num)
+			return true
+		else 
+			fib_array << fib_array[-1]+fib_array[-2]
+		end 
+
+	end 
+	return false 
 
 end
 
@@ -24,7 +39,13 @@ end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+puts is_fibonacci?(5) == true
 
+puts is_fibonacci?(4) == false
+
+puts is_fibonacci?(8670007398507948658051921) == true 
+
+puts is_fibonacci?(8670007398507948658051922) == false
 
 
 
